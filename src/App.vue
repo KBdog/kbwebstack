38ec4033bbc86e1fed5d46fabbc691c6674e9c23
KBdog <1246450339@qq.com>
2021-11-18 11:24:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,34 @@
       <router-view></router-view>
   </div>
 </template>
+<script>
+  export default{
+    created(){
+        //check console
+        // setInterval(() => {
+        //     debugger;
+        // }, 100);
+        
+        // //prohitbit f12 keyEvent
+        // document.addEventListener('keydown', function(event){
+        //     return 123 != event.keyCode || (event.returnValue = false)
+        // });
+        // //prohitbit right mouse click
+        // document.addEventListener('contextmenu', function(event){
+        //     return event.returnValue = false
+        // })
+
+        this.$nextTick(() => {
+          // prohitbit right mouse click
+          document.oncontextmenu = new Function('event.returnValue=false')
+          // prohitbit choose
+          document.onselectstart = new Function('event.returnValue=false')
+          // prohitbit console
+          document.onkeydown = new Function('event.returnValue=false')
+        })
+    }
+  }
+</script>
 <style lang="scss">
   /*@import "./assets/css/main.css";*/
     *{

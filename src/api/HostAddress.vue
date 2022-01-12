2633ee45f68406b662efb2d8294cee525ef500b3
KBdog <1246450339@qq.com>
2022-01-13 00:52:22 +0800

    update api

--- src/api/HostAddress.vue
+++ src/api/HostAddress.vue
@@ -4,13 +4,11 @@
 
 <script>
     //当前网络ip地址全局变量
-    // const localhostServer="api.kbwebstack.site";
-    const localhostServer="localhost:8084";
+    const localhostServer="api.kbwebstack.site";
     //后端端口号
     const port=8888;
     //协议类型
-    // const scheme="https";
-    const scheme="http";
+    const scheme="https";
     export default {
         name: "ApiAddress",
         host:localhostServer,
